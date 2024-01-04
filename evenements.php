30f153f609de0f311bef340ab8b93484800bb43a
TabaraD <tabara20.dia08@gmail.com>
2024-01-04 17:56:14 +0100

    Update evenements.php

--- evenements.php
+++ evenements.php
@@ -22,33 +22,46 @@
   <main>
       <H1>Nos évènements</H1>
       <section>
-        <div class="carte">
-      <img class="affiche" src="public/images/Sensibilise.png" alt="affiche sensibilise">
+      <?php 
+
+$nos_evenements = [
+    1 => [
+      "intitulé"=>"Transcendant en visite dans le lycée de la Mare Carré",
+      "date"=>"le samedi 20 janvier 2024",
+    "programme"=>["atelier"=>"atelier","jeux"=>"jeux : échange tes stéréotypes","présentation sur la transidentité"=>"présentation sur la transidentité"],
+    "cadeaux"=>"des t-shirts transcendants",],
+    //...Pour mettre d'autres évènements
+  ];
+    ?>
       <?php
-      $evenements =[
-         "intitulé"=>"Transcendant en visite dans le lycée de la Mare Carré",
-         "date"=>"le samedi 20 janvier 2024",
-         "programme"=>["atelier"=>"atelier","jeux : échange tes stéréotypes"=>"jeux : échange tes stéréotypes","présentation sur la transidentité"=>"présentation sur la transidentité"],
-         "cadeaux"=>"des t-shirts transcendants",
-        // ... pour mettre d'autres évènements.
-      ]; ?>
-      <p><?=$evenements['intitulé'] ?></p><br>
-      <p><?= $evenements['date'] ?></p><br>
-      <p><?= $evenements['programme']['atelier'] ?></p><br>
-      <p><?= $evenements['programme']['jeux : échange tes stéréotypes'] ?></p><br>
-      <p><?= $evenements['programme']['présentation sur la transidentité'] ?></p><br>
-      <p><?= $evenements['cadeaux'] ?></p>
-      <p><a href="http://localhost/SAE/evenements.php/#merch">(voir plus)</a></p>
-      </div>
+foreach ($nos_evenements as $evenement) : ?>
+    <div class="carte">
+    <figure><img class="affiche" src="public/images/Sensibilise.png" alt="affiche sensibilise"></figure>
+    <div class="infos">
+      <p><?=$evenement['intitulé'] ?></p><br>
+      <p><?= $evenement['date'] ?></p><br>
+      <p><?= $evenement['programme']['atelier'] ?></p><br>
+      <p><?= $evenement['programme']['jeux'] ?></p><br>
+      <p><?= $evenement['programme']['présentation sur la transidentité'] ?></p><br>
+      <p><?= $evenement['cadeaux'] ?></p>
+      <p><a href="#merch">(voir plus)</a></p></div>
+    </div>
+  <?php endforeach ?>
     </section>
+
+
     <section id="à_venir">
       <p>D'autres évènements seront bientôt disponibles...</p>
     </section>
+
+
     <section>
       <div id="merch"><p>Merch offerte:</p>
       <img class="shirt" src="public/images/t-shirt_transcendant.png" alt="T-shirt Transcendant">
       </div>
     </section>
+
+
     <section class="sticky">
         <div class="bubbles">
             <div class="bubble"></div>
