2cd4fb41b28be40761fc2088669752b2bef0d2fa
Dhénaux Bryan <dhenaux.bryan@gmail.com>
2024-01-09 15:21:55 +0100

    UPDATE QUISOMMESNOUS

---
+++ Quisommesnous/Bryan/Bryan.php
@@ -0,0 +1,85 @@
+
+<!DOCTYPE html>
+
+<html>
+    
+<head>
+    <meta title="Bryan.co" />
+    <meta charst="UTF-8" />
+    <link href="stylesheet.css" rel="stylesheet" />
+</head>
+
+<body>
+    
+    <header>
+        </br>
+        <h1>Bryan's Website</h1>
+        <h3># No limits #</h3>
+        </br>
+    </header>
+
+    <main>
+        <p></p>
+    </br>
+        <div id="image">
+        <img src="bryan2.png" />
+        </div>
+        <p></p>
+    </br>
+        <h1>Biographie</h1>
+        <h2>Hello ! Je m'appelle Bryan Dhénaux et je suis un passionné de sports mécaniques qui touche les véhicules motorisés.
+        J'ai commencé le modélisme R/C à l'age de 17 ans en loisir 1/10 chez l'entreprise Traxxas avec le Slash 2WD en 2021.</h2>
+
+        <h2>En 2022, après mettre lancé avec Arrma RC en 1/7 Speed bashing (discipline des speedrun), je me suis inscrit à 18
+        ans au club de modélisme CLM94 afin d'approfondir mes connaissances en mécaniques.</h2>
+
+        <h2>Aujourd'hui, j'évolue en 1/8 TT électrique avec Sworkz et en 1/8 rallygame électrique avec XRAY RC.</h2>
+
+        <h2>Mon équipe FireFly Racing recrute des pilotes loisir et compétition débutant et cible un public jeune, le but est
+        de montrer ce que les jeunes sont capables de faire avec moins de connaissances que les pilotes célèbres. Nous acceptons
+        toutes les catégories : TTs, pistes, bateaux, hélicoptères ou encore avions.</h2>
+    </br>
+        <p></p>
+    </br>
+        <h1>Galerie</h1>
+        <p></p>
+        <img src="Sworkz S35.jpg" />
+        <p></p>
+    </br>
+        <img src="ArrmaLimitless.jpg" />
+        <p></p>
+    </br>
+        <p></p>
+    </br>
+        <p></p>
+    </br>
+        <p></p>
+    </br>
+    </main>
+
+    <footer>
+       <p></p>
+       </br>
+       <h1>Ma chaîne YouTube</h1>
+       <div id="youtube image">
+       <img src="channel profile.jpg" />
+       </div>
+       <h2>Mes videos !</h2>
+       <a href="https://www.youtube.com/channel/UCnhQX8MmEqpL8Prpv2cJUaQ">C'est ici !</a>
+    </br>
+    <p></p>
+    </br>
+       <h1>Mon sponsor</h1>
+       <img src="donut racing logo.png" />
+       <h2>Fournisseur de matériel de modélisme</h1>
+       <a href="https://www.donuts-racing.com/fr/">Voir plus</a>
+       <p></p>
+    </br>
+       <p></p>
+    </br>
+       <p>Cette page HTML a été réalisé par Bryan Dhénaux (MMI, IUT Sénart-Fontainebleau) - 2023, Tous droits réservés</p>
+       <p></p>
+    </br> 
+    </footer>
+
+</body>
---
+++ Quisommesnous/liste.php
@@ -0,0 +1,34 @@
+<?php 
+
+$groupe = [
+    [
+       'prenom' => 'Amandine',
+       'nom' => 'Batrikian',
+       'photo' => 'amandine.png',
+       'page_perso' => 'Amandine/Amandine.php',
+    ],
+    [
+       'prenom' => 'Célestin',
+       'nom' => 'Bouy',
+       'photo' => 'celestin.jpg.png',
+       'page_perso' => 'Célestin/Célestin.php',
+    ],
+    [
+       'prenom' => 'Yasmine',
+       'nom' => 'Oulmi',
+       'photo' => 'defaut.png',
+       'page_perso' => 'Bryan/Bryan.php',
+   ],
+   [
+       'prenom' => 'Bryan',
+       'nom' => 'Dhénaux',
+       'photo' => 'bryan.jpg',
+       'page_perso' => 'Bryan/Bryan.php',
+   ],
+   [
+       'prenom' => 'Tabara',
+       'nom' => 'Dia',
+       'photo' => 'tabara.jpg.png',
+       'page_perso' => 'page_tabara/Tabara.php',
+   ],
+];
