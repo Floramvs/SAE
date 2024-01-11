12665c2420dddf3cf70e2d23d5dd7791bb97eb0d
TabaraD <tabara20.dia08@gmail.com>
2024-01-11 12:52:40 +0100

    update informer

--- informer.php
+++ informer.php
@@ -7,29 +7,11 @@
     <link rel="stylesheet" href="public/css/informer.css">
 </head>
 <body>
-<header>
-        <a href="http://localhost/SAE/SAE.php"><img id="logo" src="public/images/logo transcendant.png" alt="logo transcendant"></a>
-        <nav>
-            <ul>
-                <li><a id="accueil" href="accueil.php">Accueil</a></li>
-                <li><a id="membre" href="membres.php">Qui sommes-nous ?</a></li>
-                <li><a id="informer" href="informer.php">S'informer</a></li>
-                <li><a id="evenement" href="evenements.php">Evènements</a></li>
-                <li><a id="contact" href="contact.php">Contact</a></li>
-            </ul>
-        </nav>  
-    </header>
+<?php include "app/view/common/header.php" ?>
     <main>
         <h1>Lexique</h1>
         <h2>Pas mal de termes sont en anglais, certains de ces termes ont été traduit (si c'est le cas, nous
              mettrons la traduction à côté), mais sont moins utilisés que leurs homologues en anglais.</h2>
-        <section>
-            <div class="deroulant">
-                <button class="menubutton">Menus</button>
-                <div class="menu_lexique">
-                <a href="">Genres et pronoms</a>
-            </div>
-        </section>
             <?php 
             $lexique_genres_pronoms =[
                 'menus' => [
@@ -44,43 +26,32 @@
                  'LGBTQIA+'=>'LGBTQIA+',]
                 ];
             ?>
+            <section>
+            <div class="deroulant">
+                <button class="menubutton">Menus</button>
+                <?php 
+            foreach ($lexique_genres_pronoms['menus'] as $menu) : ?>
+                <div class="menu_lexique" ><a href=""><p><?= $menu ?></p></a></div>
+            <?php endforeach ?>
+            </div>
+        </section>
             <?php 
             foreach ($lexique_genres_pronoms['terme'] as $definition) : ?>
                 <div class="mot" ><a href=""><p><?= $definition ?></p></a></div>
             <?php endforeach ?>
 
-        <section class="sticky">
-        <div class="bubbles">
-            <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div>
-          <div class="bubble"></div> 
-        </div>
-      </section>
+            <section class="sticky">
+    <?php
+$nbBubbles = 27;
+
+echo '<div class="bubbles">';
+for ($i = 0; $i < $nbBubbles; $i++) {
+    echo '<div class="bubble"></div>';
+}
+echo '</div>';
+?>
+        </section>
     </main>
+    <?php include "app/view/common/footer.php" ?>
 </body>
 </html>
