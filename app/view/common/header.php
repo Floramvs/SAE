b084db64e1697d022213eaf58ea36c39ac8ad922
TabaraD <tabara20.dia08@gmail.com>
2024-01-12 15:16:28 +0100

    mise à jour header

--- app/view/common/header.php
+++ app/view/common/header.php
@@ -12,7 +12,7 @@
 			<a href="accueil.php"><img id="logo" src="public/images/logo transcendant"></a>
 			
 			<ul>
-				<li><a href="accueil.php" class="active">Accueil</a></li>
+				<li><a href="accueil.php">Accueil</a></li>
 				<li><a href="équipe.php">Membres</a></li>
 				<li><a href="informer.php">S'informer</a></li>
 				<li><a href="evenements.php">Evenements</a></li>
