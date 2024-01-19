d3a4513ccec494693a9e34e34dfbba93cef672af
TabaraD <tabara20.dia08@gmail.com>
2024-01-19 15:28:32 +0100

    maj équipe

--- équipe.php
+++ équipe.php
@@ -27,7 +27,7 @@ include 'liste.php';?>
 
         <main>
 
-    <h1 id="title">Qui sommes-nous ?</h1>
+    <h1 id="titre">Qui sommes-nous ?</h1>
 
     <p class="description">Bonjour, Nous sommes un collectif ayant pour but d'éduquer et aider autrui vis à vis des stéréotypes de genres et la transphobie, qui affectent tout le monde d'une manière ou d'une autre au quotidien.</br><span>Comment ?</span>A travers nos visites dans les collèges, lycées et universités et notre lexique varié, ayez accès facilement et rapidement à des renseignements sur l'identité de genre dans un cadre d'écoute et de bienveillance.</p>
 
@@ -39,9 +39,12 @@ include 'liste.php';?>
 
     <p class="description">La forme est une bulle entrain de s'éclater, représentant le fait de briser des codes et sortir des cases des stéréotypes de genre. Les "vagues" crée par l'explosion représente une fluidité dans les identités de genre. Et les couleurs proviennent de celles du drapeau trans. Le violet représente l'entre les deux et est en rapport avec la fluidité et la non-binarité.</p>
 
+    <section id="equipe">
+
     <p class="titre">Notre équipe</p>
 
-    <?php foreach ($groupe as $eleve) : ?>
+
+     <?php foreach ($groupe as $eleve) : ?>
 
       <div class="carte">
 
@@ -60,7 +63,10 @@ include 'liste.php';?>
           <p class="nom"><?= $eleve['prenom'] ?> <span><?= $eleve['nom'] ?></span></p></a>
         </div>
 </div>
-      <?php endforeach ?>
+      <?php endforeach ?> 
+    </section>
+
+    
   </main>
 
 
